cce65b93dc287a6a21cd8b8cf5c39ae7850f6f7b
sajeel ahmed <sajeelsam35@gmail.com>
2025-02-25 08:24:09 +0500

    qucik sort issue fixed

--- quickSort.cpp
+++ quickSort.cpp
@@ -1,42 +1,46 @@
 #include <iostream>
 using namespace std;
 
-int partition(int *arr, int low, int high)
+int partition(int arr[], int lb, int up)
 {
-    int piviot = low;
-    int i = low + 1, j = high;
-    do
+    int pivot = arr[lb];
+    int i = lb, j = up;
+
+    while (i < j)
     {
-        while (arr[i] <= piviot)
-        {
+        while (i <= up && arr[i] <= pivot)
             i++;
-        }
-        while (arr[j] >= piviot)
-        {
+        while (j >= lb && arr[j] > pivot)
             j--;
-        }
 
         if (i < j)
-        {
-            int temp = arr[i];
-            arr[i] = arr[j];
-            arr[j] = temp;
-        }
-    } while (i < j);
-    int temp = arr[low];
-    arr[low] = arr[high];
-    arr[high] = temp;
+            swap(arr[i], arr[j]);
+    }
 
+    swap(arr[lb], arr[j]);
     return j;
 }
 
 void quickSort(int *arr, int low, int high)
 {
-    int partitionIndex;
     if (low < high)
     {
-        partitionIndex = partition(arr, low, high);
+        int partitionIndex = partition(arr, low, high);
         quickSort(arr, low, partitionIndex - 1);
         quickSort(arr, partitionIndex + 1, high);
     }
+}
+
+int main()
+{
+    int arr[] = {5, 3, 9, 1};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    quickSort(arr, 0, n - 1);
+
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+
+    cout << endl;
+    return 0;
 }
